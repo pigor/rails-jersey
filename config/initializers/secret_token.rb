a62d4bd684b02c7045fa9ff05f2b3df608b22249
Paulo Igor <piagodinho@gmail.com>
2013-08-19 13:14:23 -0300

    Criando projeto de exemplo para integrar com uma aplicação java

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJersey::Application.config.secret_token = '62de248e0248eac175786efc2d39f1410a9c8a3bab4a4104dff4ea431daefcda3cc6308262fc02eba7ef6551c5d04af233b1cfab6bf3e4d7c0bb0ce717400dc8'
